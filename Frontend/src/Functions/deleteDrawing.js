9f2e0aee0f57a84b5fca3173de368a3c5d701704
salman_farshy <137337146+salmanfarshy@users.noreply.github.com>
2024-09-17 21:43:04 +0600

    added toast in deleteDrawing func

--- Frontend/src/Functions/deleteDrawing.js
+++ Frontend/src/Functions/deleteDrawing.js
@@ -1,12 +1,23 @@
 import apiRequest from "./apiRequest";
+import { useToast } from "../hooks/use-toast.js";
 
 const deleteDrawing = async ({ id, setDeleteLoading, navigate }) => {
+  const { toast } = useToast();
   setDeleteLoading(true);
+  
   await apiRequest.post("/delete-drawing", {
     id,
   });
+  
   setDeleteLoading(false);
   navigate("/");
+   toast({
+      className: "bg-gray-50 text-green-700 fixed top-5 right-5 w-80",
+      variant: "success",
+      title: "Success",
+      description: "Drawing has been deleted.",
+      duration: 2000,
+    });
 };
 
 export default deleteDrawing;
